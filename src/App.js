7addd08bdf211467823b012055f1677c58830aa7
CrisioDev <crisioletsplay@gmail.com>
2019-03-07 12:30:53 +0100

    Inital

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
+  state  = {
+    persons: [
+      {name: 'Christopher',age:'28'},
+      {name: 'Gina',age:'25'},
+      {name: 'Calvin',age:'20'}
+    ]
+  }
+
+  switchNameHandler = (newName) => {
+    // console.log('was clicked');
+    this.setState({
+      persons: [
+        {name: newName,age:'23'},
+        {name: 'GinaMie',age:'25'},
+        {name: 'Calvin',age:'20'}
+      ]
+    });
+  }
+
+  nameChangedHandler = (event) =>{
+    this.setState({
+      persons: [
+        {name: event.target.value, age:'23'},
+        {name: 'GinaMie',age:'25'},
+        {name: 'Calvin',age:'20'}
+      ]
+    });
+  }
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid #eee',
+      padding: '8px',
+      cursor: 'pointer'
+    };
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Hi, I'm a React App!</h1>
+        <p>Crazy JSX Stuff is goin' on!</p>
+        <button 
+          style={style}
+          onClick={() => this.switchNameHandler('Crisio')} >Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}
+          click={this.switchNameHandler.bind(this, 'Christopher')}
+          changed={this.nameChangedHandler}/>
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          >Loves Animes</Person>
       </div>
     );
   }
